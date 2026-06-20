18e98f1d23a0524ab9f5ad241a129ad1dd75f08b
v.studitckih <vika.boltaeva@inbox.ru>
2026-06-20 16:08:04 +0400

    Fix: add MongoDB certificate

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,8 +1,19 @@
 import mongoose from 'mongoose';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URL);
+        const certPath = path.join(__dirname, 'root.crt');
+        const mongoUrl = process.env.MONGODB_URL.replace(
+            'tlsCAFile=./root.crt',
+            `tlsCAFile=${certPath}`
+        );
+        
+        await mongoose.connect(mongoUrl);
         console.log('✅ DB Connected');
     } catch (error) {
         console.log('❌ Ошибка подключения к БД:');
